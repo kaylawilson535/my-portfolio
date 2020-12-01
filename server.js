062d822b6a9fc69569c90e5c9c65dc6cc290954a
kaylawilson535 <42094645+kaylawilson535@users.noreply.github.com>
2020-11-30 17:12:50 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+const indexRouter = require('./controllers/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Running on 3000');
+})
