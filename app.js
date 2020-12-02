0197f4f02e90e4ad28f71b1b6968c1560ddd6f5e
kaylawilson535 <42094645+kaylawilson535@users.noreply.github.com>
2020-12-01 16:58:47 -0800

    Added basic styling to home page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express()
 const bodyParser = require('body-parser')
 
 const indexRouter = require('./controllers/index')
+const designRouter = require('./controllers/design')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -14,5 +15,6 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 app.use('/', indexRouter)
+app.use('/designs', designRouter)
 
 app.listen(process.env.PORT || 3000)
