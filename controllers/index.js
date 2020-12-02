3a7f89976d5038f5d8c8fb76e565e48911755653
kaylawilson535 <42094645+kaylawilson535@users.noreply.github.com>
2020-12-02 00:27:34 -0800

    Backup before adding domain

--- controllers/index.js
+++ controllers/index.js
@@ -4,5 +4,20 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.render('index')
 })
+router.get('/endofexile', (req, res) => {
+    res.render('end-of-exile')
+})
+router.get('/instructar', (req, res) => {
+    res.render('instructAR')
+})
+router.get('/tpfrenzy', (req, res) => {
+    res.render('tp-frenzy')
+})
+router.get('/3dmodels', (req, res) => {
+    res.render('3d-models')
+})
+router.get('/uidesigns', (req, res) => {
+    res.render('ui-designs')
+})
 
 module.exports = router
